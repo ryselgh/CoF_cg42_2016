9194322e41f3bfcd0d6c5fc9650e912c6daf9efe
rysel <6789973@gmail.com>
2016-04-28 14:51:13 +0200

    early version of Game.java and Player.java

--- src/main/java/gamelogic/Player.java
+++ src/main/java/gamelogic/Player.java
@@ -1,219 +1,210 @@
-package gamelogic ;
+package gamelogic;
 
+import java.util.ArrayList;
 import board.Assistant;
-import board.Bonus;
 import board.Emporium;
+import board.Map;
 import board.Pawn;
 import decks.PermitsCard;
 import decks.PoliticsCard;
 import model.PawnColor;
 import model.Track;
 
-/**
- * <!-- begin-user-doc -->
- * <!--  end-user-doc  -->
- * @generated
- */
 
-public class Player
-{
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	private Emporium[] availableEmporiums;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	private Assistant[] availableAssistants;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	private PermitsCard[] permits;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	private PoliticsCard[] hand;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	private Pawn[] pawn;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
+public class Player {
+
+	private ArrayList<Emporium> availableEmporiums;
+	private ArrayList<Assistant> availableAssistants;
+	private ArrayList<PermitsCard> permits;
+	private ArrayList<PoliticsCard> hand;
+	private Pawn pawn;
 	private MainAction mainAction;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
 	private SpeedAction speedAction;
-	
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
 	private int coins;
-	
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
 	private int score;
-	
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
+	 * Constructs an object of type Player
+	 * 
+	 * @param emporiums
+	 *            Quantity of emporiums to give to the player
+	 * @param assistants
+	 *            Quantity of assistants to give to the player
+	 * @param color
+	 *            The chosen color of the player
 	 */
-	
-	public Player(Emporium[] e, Assistant[] a) {
-		super();
-		// TODO construct me	
+
+	public Player() {
+		mainAction = new MainAction();
+		speedAction = new SpeedAction();
 	}
-	
+
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
+	 * Gives a specific amount of assistants to this player
+	 * 
+	 * @param qty
+	 *            The quantity of assistants you want to add to this player
 	 */
-	
+
+	public void addAssistant(int qty) {
+		for (int i = 0; i < qty; i++) {
+			availableAssistants.add(Map.getAssistant());
+		}
+	}
+
+	/**
+	 * @return the available emporiums
+	 */
+	public ArrayList<Emporium> getAvailableEmporiums() {
+		return availableEmporiums;
+	}
+
+	/**
+	 * @param availableEmporiums
+	 *            the available emporiums to set
+	 */
+	public void setAvailableEmporiums(ArrayList<Emporium> availableEmporiums) {
+		this.availableEmporiums = availableEmporiums;
+	}
+
+	/**
+	 * @return player's color
+	 */
+
 	public PawnColor getColor() {
-		// TODO implement me
-		return PawnColor.RED;	
+		return this.getPawn().getColor();
 	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public Pawn getPawn(Track t) {
-		// TODO implement me
-		return null;	
-	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public void ObtainBonus(Bonus b) {
-		// TODO implement me	
-	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public PoliticsCard[] getHand() {
-		// TODO implement me
-		return null;	
-	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public void addPolitics(PoliticsCard poc) {
-		// TODO implement me	
-	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public void addPermits(PermitsCard pec) {
-		// TODO implement me	
-	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public void sellAssistant(Assistant a) {
-		// TODO implement me	
-	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public void sellPoliticsCard(PoliticsCard polc) {
-		// TODO implement me	
-	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public void sellPermitsCard(PermitsCard perc) {
-		// TODO implement me	
-	}
-	
-}
 
+	/**
+	 * @return player's pawn
+	 */
+
+	public Pawn getPawn() {
+		return pawn;
+	}
+
+	/**
+	 * Shows cards in the player's hand
+	 */
+
+	public ArrayList<PoliticsCard> getHand() {
+		return hand;
+	}
+
+	/**
+	 * @return the coins
+	 */
+	public int getCoins() {
+		return coins;
+	}
+
+	/**
+	 * @param coins
+	 *            the coins to set
+	 */
+	public void setCoins(int coins) {
+		this.coins = coins;
+	}
+
+	/**
+	 * @return the score
+	 */
+	public int getScore() {
+		return score;
+	}
+
+	/**
+	 * @param score
+	 *            the score to set
+	 */
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	/**
+	 * Adds a politics card to the hand
+	 * 
+	 * @param poc
+	 *            a politics card, typically obtained by draw or buy actions
+	 */
+
+	public void addPolitics(PoliticsCard poc) {
+		hand.add(poc);
+	}
+
+	/**
+	 * Removes a politics card from the hand
+	 * 
+	 * @param index
+	 *            the index of the card in the ArrayList
+	 */
+
+	public void removePolitics(int index) {
+		hand.remove(index);
+	}
+
+	/**
+	 * Assigns a permit card to the player
+	 * 
+	 * @param pec
+	 *            a permit, typically obtained by satisfying a council or buy
+	 *            action
+	 */
+
+	public void addPermits(PermitsCard pec) {
+		permits.add(pec);
+	}
+
+	/**
+	 * @return the mainAction
+	 */
+	public MainAction getMainAction() {
+		return mainAction;
+	}
+
+	/**
+	 * @return the speedAction
+	 */
+	public SpeedAction getSpeedAction() {
+		return speedAction;
+	}
+
+	
+	// <--------- MARKET BEGIN ---------->
+	
+	/**
+	 * Sell an assistant
+	 * 
+	 * @param a
+	 *            the assistant that is being sold
+	 */
+
+	public void sellAssistant(Assistant a) {
+		// TODO implement me
+	}
+
+	/**
+	 * Sell a politics card
+	 * 
+	 * @param polc
+	 *            the politics card that is being sold
+	 */
+
+	public void sellPoliticsCard(PoliticsCard polc) {
+		// TODO implement me
+	}
+
+	/**
+	 * Sell a permit
+	 * 
+	 * @param perc
+	 *            the permit that is being sold
+	 */
+
+	public void sellPermitsCard(PermitsCard perc) {
+		// TODO implement me
+	}
+	
+	// <--------- MARKET END ---------->
+
+}
