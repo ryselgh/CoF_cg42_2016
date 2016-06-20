154c1c3c0a3d09c94c15d1bca85b89670575b59c
rysel <6789973@gmail.com>
2016-06-20 16:49:44 +0200

    continued lobby, added a form for creating a new room

--- client/src/main/java/com/client/view/gui/MainMenu.java
+++ client/src/main/java/com/client/view/gui/MainMenu.java
@@ -7,10 +7,13 @@ import java.util.ResourceBundle;
 import javafx.animation.FadeTransition;
 import javafx.animation.ScaleTransition;
 import javafx.animation.TranslateTransition;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
@@ -45,6 +48,24 @@ public class MainMenu implements javafx.fxml.Initializable {
 	private Button btnSelectMap;
 	@FXML
 	private ImageView lobbyMask;
+	@FXML
+	private ImageView formNewRoom;
+	@FXML
+	private TextField txtRoomName;
+	@FXML
+	private ChoiceBox<String> choiceMinPl;
+	private ObservableList<String> minList = FXCollections.observableArrayList("2","3","4","5","6","7","8");
+	@FXML
+	private ChoiceBox<String> choiceMaxPl;
+	private ObservableList<String> maxList = FXCollections.observableArrayList("2","3","4","5","6","7","8");
+	@FXML
+	private ChoiceBox<String> choiceMap;
+	private ObservableList<String> mapList = FXCollections.observableArrayList("Default","Map2","Map3","Map4","Map5","Map6","Map7","Map8");
+	@FXML
+	private Button btnCreateNewRoom;
+	@FXML
+	private Button btnCancelNewRoom;
+	
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -71,6 +92,14 @@ public class MainMenu implements javafx.fxml.Initializable {
 			Label txtStartGame = new Label("Start Game");
 			txtStartGame.setStyle("-fx-font-size: 10pt; -fx-font-family: \"PerryGothic\"; -fx-text-fill: #A11212; -fx-effect: dropshadow(three-pass-box, rgba(209,181,82,0.9), 2, 0.8, 0, 0);");
 			btnStartGame.setGraphic(txtStartGame);
+			
+			Label txtCreate = new Label("Create");
+			txtCreate.setStyle("-fx-font-size: 10pt; -fx-font-family: \"PerryGothic\"; -fx-text-fill: #A11212; -fx-effect: dropshadow(three-pass-box, rgba(209,181,82,0.9), 2, 0.8, 0, 0);");
+			btnCreateNewRoom.setGraphic(txtCreate);
+			
+			Label txtCancel = new Label("Cancel");
+			txtCancel.setStyle("-fx-font-size: 10pt; -fx-font-family: \"PerryGothic\"; -fx-text-fill: #A11212; -fx-effect: dropshadow(three-pass-box, rgba(209,181,82,0.9), 2, 0.8, 0, 0);");
+			btnCancelNewRoom.setGraphic(txtCancel);
 
 			//Nickname selection
 			btnNickname.setDisable(true);
@@ -87,7 +116,18 @@ public class MainMenu implements javafx.fxml.Initializable {
 			btnLeaveRoom.setLayoutX(1025);
 			btnSelectMap.setLayoutX(1025);
 			btnStartGame.setLayoutX(1025);
-
+			
+			//New Room form
+			choiceMinPl.setItems(minList);
+			choiceMinPl.setDisable(true);
+			choiceMaxPl.setItems(maxList);
+			choiceMaxPl.setDisable(true);
+			choiceMap.setItems(mapList);
+			choiceMap.setDisable(true);
+			btnCreateNewRoom.setDisable(true);
+			btnCancelNewRoom.setDisable(true);
+			txtRoomName.setDisable(true);
+			
 			playMainSoundtrack();
 			animateNickname();
 
@@ -104,11 +144,57 @@ public class MainMenu implements javafx.fxml.Initializable {
 			btnNickname.setDisable(false);
 		}
 	}
+	
+	public void checkCreateRoom(){
+		if((!txtRoomName.getText().matches("[A-Za-z0-9]*") || txtRoomName.getText().length()<5 || txtRoomName.getText().length()>13) ||
+			choiceMinPl.getValue() == null || choiceMaxPl.getValue() == null || choiceMap.getValue() == null ||
+			Integer.parseInt(choiceMaxPl.getValue())<Integer.parseInt(choiceMinPl.getValue()) || formNewRoom.getOpacity() == 0.0){
+			btnCreateNewRoom.setDisable(true);
+		}else{
+			btnCreateNewRoom.setDisable(false);
+			btnCreateNewRoom.setOpacity(1.0);
+		}
+	}
+	
+	public void newRoomForm(){
+		if(formNewRoom.getOpacity() == 0.0){
+			animateNewRoomForm();
+			btnCreateNewRoom.setDisable(true);
+			btnCancelNewRoom.setDisable(false);
+			txtRoomName.setDisable(false);
+			choiceMinPl.setDisable(false);
+			choiceMaxPl.setDisable(false);
+			choiceMap.setDisable(false);
+		}
+	}
+	
+	public void createNewRoom() {
+		
+	}
+	
+	public void cancelNewRoom() {
+		if(formNewRoom.getOpacity() == 1.0){
+			invertAnimateNewRoomForm();
+			btnCreateNewRoom.setDisable(true);
+			btnCancelNewRoom.setDisable(true);
+			txtRoomName.setDisable(true);
+			choiceMinPl.setDisable(true);
+			choiceMaxPl.setDisable(true);
+			choiceMap.setDisable(true);
+		}
+	}
 
 	public void playMainSoundtrack(){
 		Media sound = new Media(getClass().getResource("mp3/MainTheme.mp3").toString());
 		MediaPlayer mediaPlayer = new MediaPlayer(sound);
 		mediaPlayer.play();
+		mediaPlayer.setOnEndOfMedia(new Runnable() {
+            @Override public void run() {
+            	Media sound = new Media(getClass().getResource("mp3/MainContinues.mp3").toString());
+        		MediaPlayer mediaPlayer = new MediaPlayer(sound);
+        		mediaPlayer.play();
+            }
+        });
 	}
 	
 	public void animateLobby(){
@@ -218,5 +304,83 @@ public class MainMenu implements javafx.fxml.Initializable {
 				});
 			}
 		});
+	}
+	
+	private void animateNewRoomForm() {
+		
+		FadeTransition ft = new FadeTransition(Duration.millis(1000), formNewRoom);
+		ft.setFromValue(0.0);
+		ft.setToValue(1.0);
+		ft.play();
+		
+		FadeTransition ft2 = new FadeTransition(Duration.millis(1000), btnCreateNewRoom);
+		ft2.setFromValue(0.0);
+		ft2.setToValue(0.5);
+		ft2.play();
+		
+		FadeTransition ft3 = new FadeTransition(Duration.millis(1000), btnCancelNewRoom);
+		ft3.setFromValue(0.0);
+		ft3.setToValue(1.0);
+		ft3.play();
+		
+		FadeTransition ft4 = new FadeTransition(Duration.millis(1000), choiceMinPl);
+		ft4.setFromValue(0.0);
+		ft4.setToValue(1.0);
+		ft4.play();
+		
+		FadeTransition ft5 = new FadeTransition(Duration.millis(1000), choiceMaxPl);
+		ft5.setFromValue(0.0);
+		ft5.setToValue(1.0);
+		ft5.play();
+		
+		FadeTransition ft6 = new FadeTransition(Duration.millis(1000), choiceMap);
+		ft6.setFromValue(0.0);
+		ft6.setToValue(1.0);
+		ft6.play();
+		
+		FadeTransition ft7 = new FadeTransition(Duration.millis(1000), txtRoomName);
+		ft7.setFromValue(0.0);
+		ft7.setToValue(1.0);
+		ft7.play();
+		
+	}
+	
+private void invertAnimateNewRoomForm() {
+		
+		FadeTransition ft = new FadeTransition(Duration.millis(1000), formNewRoom);
+		ft.setFromValue(1.0);
+		ft.setToValue(0.0);
+		ft.play();
+		
+		FadeTransition ft2 = new FadeTransition(Duration.millis(1000), btnCreateNewRoom);
+		ft2.setFromValue(btnCreateNewRoom.getOpacity());
+		ft2.setToValue(0.0);
+		ft2.play();
+		
+		FadeTransition ft3 = new FadeTransition(Duration.millis(1000), btnCancelNewRoom);
+		ft3.setFromValue(1.0);
+		ft3.setToValue(0.0);
+		ft3.play();
+		
+		FadeTransition ft4 = new FadeTransition(Duration.millis(1000), choiceMinPl);
+		ft4.setFromValue(1.0);
+		ft4.setToValue(0.0);
+		ft4.play();
+		
+		FadeTransition ft5 = new FadeTransition(Duration.millis(1000), choiceMaxPl);
+		ft5.setFromValue(1.0);
+		ft5.setToValue(0.0);
+		ft5.play();
+		
+		FadeTransition ft6 = new FadeTransition(Duration.millis(1000), choiceMap);
+		ft6.setFromValue(1.0);
+		ft6.setToValue(0.0);
+		ft6.play();
+		
+		FadeTransition ft7 = new FadeTransition(Duration.millis(1000), txtRoomName);
+		ft7.setFromValue(1.0);
+		ft7.setToValue(0.0);
+		ft7.play();
+		
 	}
 }
